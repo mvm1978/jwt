c7de7e056fa9bd81e02f6a3cb0e742caddf19b37
unknown <mvm031978@gmail.com>
2017-12-26 19:02:39 -0500

    refer to Virtual Library when sending emails

--- resources/views/emails/passwordRecovery.php
+++ resources/views/emails/passwordRecovery.php
@@ -6,7 +6,7 @@ $user = trim($info['first_name'] . ' ' . $info['last_name']);
 $comma = $user ? ',' : NULL; ?>
 
 <h3>Hello<?php echo $comma . ' ' . $user; ?></h3>
-<p>You receive this email because you have requested password recovery at MVM</p>
+<p>You receive this email because you have requested password recovery at Virtual Library</p>
 <p>Your username is <strong><?php echo $info['username']; ?></strong></p>
 <p>Follow this link to reset you password <a href="<?php echo $ulr; ?>" target="_blank"><?php echo $ulr; ?></a></p>
 <p>Note: the link will expire in 5 minutes! If you fail to change the password within 5 minutes you will have to start a new password recovery process again!</p>
--- resources/views/emails/register.php
+++ resources/views/emails/register.php
@@ -4,4 +4,4 @@ $comma = $user ? ',' : NULL;
 
 ?>
 <h3>Hello<?php echo $comma . ' ' . $user; ?></h3>
-<p>Thank you for signing up to MVM</p>
+<p>Thank you for signing up to Virtual Library</p>
